1c249d02682faa0d7a29db13a3dd533e9c8e1875
Nicolas Camacho <nicolas.camacho@zemoga.com>
2022-08-10 02:14:10 -0500

    :tada: Firts commit

---
+++ client/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  const getData = async () => {
+    const response = await fetch('http://localhost:3000/users')
+    const data = await response.json()
+    console.log(data)
+  }
+</script>
+
+<main>
+  <h1>Hello World</h1>
+  <button on:click={getData}>
+    Obtener datos
+  </button>
+</main>
+
+<style>
+
+</style>
